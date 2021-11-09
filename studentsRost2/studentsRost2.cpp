9fa11d63fd044489e14b0e823c1248bc17b7b10c
Max <msxkurko3@gmail.com>
2021-11-09 20:00:50 +0200

    Добавьте файлы проекта.

---
+++ studentsRost2/studentsRost2.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;
+#include <Windows.h>
+#include <iostream>
+
+
+int main()
+{
+	int ROST[70];
+	int NR[70];
+	int amount = 0;
+	srand(time(0));
+
+	for (int i = 0; i < sizeof(ROST) / sizeof(int); ++i) {
+		ROST[i] = 100 + rand() % 120;
+	}
+
+	for (int i = 0 ,j = 0; i < sizeof(ROST) / sizeof(int); ++i) {
+
+		if(ROST[i] < 180){
+			ROST[i] = NR[j];
+			++j;
+		}
+		amount = j;
+	}
+
+	cout << amount << endl;
+	Sleep: 10000;
+	
+}
+
